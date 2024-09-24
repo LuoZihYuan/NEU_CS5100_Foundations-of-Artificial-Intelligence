427d2abce3d8ae7593f7110588b63148725b1718
LuoZihYuan <zihyuan.luo@gmail.com>
2024-09-24 03:21:16 -0700

    :sparkles: feat(main.py): Classification task done.

--- Homework 2/_data.py
+++ Homework 2/_data.py
@@ -6,14 +6,14 @@ DATA_MODULE = "data"
 
 def load_cats(size:int = 48) -> tuple:
     cats_path = resources.files(DATA_MODULE) / "cats/cats_{}.csv".format(size)
-    data = np.genfromtxt(cats_path, dtype=np.uint8)
+    data = np.genfromtxt(cats_path, delimiter=',', dtype=np.uint8)
     x = data[:, :-1]
     y = data[:, -1]
     return x, y
 
 def load_age(size:int = 48) -> tuple:
     age_path = resources.files(DATA_MODULE) / "age/age_{}.csv".format(size)
-    data = np.genfromtxt(age_path, dtype=np.uint8)
+    data = np.genfromtxt(age_path, delimiter=',', dtype=np.uint8)
     x = data[:, :-1]
     y = data[:, -1]
     return x, y
--- Homework 2/main.py
+++ Homework 2/main.py
@@ -1,9 +1,100 @@
 import numpy as np
+from sklearn.base import BaseEstimator
 from sklearn.datasets import load_digits
 
-data = np.array([[0,0,5,13,9,1,0,0,0,0,13,15,10,15,5,0,0,3,15,2,0,11,8,0,0,4,12,0,0,8,8,0,0,5,8,0,0,9,8,0,0,4,11,0,1,12,7,0,0,2,14,5,10,12,0,0,0,0,6,13,10,0,0,0,0],
-                 [0,0,0,12,13,5,0,0,0,0,0,11,16,9,0,0,0,0,3,15,16,6,0,0,0,7,15,16,16,2,0,0,0,0,1,16,16,3,0,0,0,0,1,16,16,6,0,0,0,0,1,16,16,6,0,0,0,0,0,11,16,10,0,0,1]], dtype=np.int64)
-target = data[:, -1].astype(int, copy=False)
-flat_data = data[:, :-1]
-print(target)
-print(flat_data)
+KFOLD = 5
+
+def eval_clf(clf:BaseEstimator, grid:dict, train_x: np.ndarray, train_y:np.ndarray, test_x:np.ndarray, test_y:np.ndarray) -> None:
+    
+    from sklearn.metrics import make_scorer, f1_score, precision_score, recall_score, confusion_matrix
+    from sklearn.model_selection import GridSearchCV
+    
+    scorer = make_scorer(f1_score, average='weighted', zero_division=0)
+
+    clf_cv = GridSearchCV(clf, grid, scoring=scorer, cv=KFOLD, n_jobs=-1, verbose=10)
+    clf_cv.fit(train_x, train_y)
+    best_clf = clf_cv.best_estimator_
+    guess_y = best_clf.predict(test_x)
+
+    print(f1_score(test_y, guess_y, average='weighted', zero_division=0))
+    print(precision_score(test_y, guess_y, average='weighted', zero_division=0))
+    print(recall_score(test_y, guess_y, average='weighted', zero_division=0))
+    print(confusion_matrix(test_y, guess_y))
+
+def classification_task():
+    
+    from _data import load_cats
+    from sklearn.svm import SVC
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.model_selection import train_test_split
+
+    x, y = load_cats()
+    x = MinMaxScaler().fit_transform(x)
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1)
+
+    receipes = [(
+            LogisticRegression(penalty='l2', random_state=0, max_iter=200),
+            {
+               'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+               'C': [0.001, 0.01, 0.1, 1, 10, 100]
+            }
+        ), (
+            SVC(),
+            {
+                'C': [0.1, 1, 10, 100, 1000],
+                'gamma': [0.0001, 0.001, 0.01, 0.1, 1],
+                'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
+            }
+        ), (
+            DecisionTreeClassifier(),
+            {
+                'max_features': ['sqrt', 'log2'],
+                'ccp_alpha': [0.1, .01, .001],
+                'max_depth' : [5, 6, 7, 8, 9],
+                'criterion' :['gini', 'entropy']
+            }
+        ), (
+            MLPClassifier(activation='logistic', max_iter=400),
+            {
+                'hidden_layer_sizes': [
+                    (48,), (96,), (144,), (192,),  # Single layer configurations
+                    (48, 32), (96, 48), (144, 72),  # Two layers
+                    (48, 32, 16), (96, 48, 24), (144, 72, 36),  # Three layers
+                ],
+                'solver': ['adam', 'sgd'],  # Test different solvers
+                'alpha': [1e-4, 1e-3, 1e-2],  # L2 regularization strength
+                'learning_rate': ['constant', 'adaptive'],  # Learning rate schedule
+            }
+        ), (
+            KNeighborsClassifier(),
+            {
+                'n_neighbors': [3, 4, 5, 10],
+                'weights': ['uniform', 'distance'],
+                'leaf_size': [15, 30, 60],
+                'p': [1, 2, 3]
+            }
+        )
+    ]
+    
+    for receipe in receipes:
+        eval_clf(receipe[0], receipe[1], train_x, train_y, test_x, test_y)
+
+
+def eval_regr(regr, train_x: np.ndarray, train_y:np.ndarray, test_x:np.ndarray, test_y:np.ndarray) -> None:
+    pass
+
+def regression_task():
+    
+    from _data import load_age
+    from sklearn.model_selection import train_test_split
+
+def main():
+    classification_task()
+    regression_task()
+
+if __name__ == "__main__":
+    main()
