6a2afbc3fc4651f0ba3822017a96c82876526cad
LuoZihYuan <zihyuan.luo@gmail.com>
2024-12-09 10:52:25 -0800

    :art: style(preprocessor): change log position

--- Final Presentation/preprocessor.py
+++ Final Presentation/preprocessor.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import pandas as pd
 from re import sub, MULTILINE
-from datetime import datetime
 from nltk import download, pos_tag
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
@@ -27,19 +26,26 @@ class FAIBasePreprocessor:
         self.rm_non_word = rm_non_word
 
     def transform(self, X:pd.Series, y:pd.Series) -> tuple:
-        print("Start <Preprocess>")
+        from datetime import datetime
+        
         processed_X = X
         if self.lowercase:
+            print("{} <Preprocess> Lower (lowercase all tweets)".format(datetime.now()))
             processed_X = self._lower(processed_X)
         if self.rm_markup:
+            print("{} <Preprocess> Remove Markup (remove markup tags from all tweets)".format(datetime.now()))
             processed_X = self._rm_markup(processed_X)
         if self.rm_url:
+            print("{} <Preprocess> Remove URL".format(datetime.now()))
             processed_X = self._rm_url(processed_X)
         if self.rm_non_word:
+            print("{} <Preprocess> Remove Non-word".format(datetime.now()))
             processed_X = self._rm_non_word(processed_X)
         if self.lemmatize:
+            print("{} <Preprocess> Lemmatize (turn words into their original form)".format(datetime.now()))
             processed_X = self._lemmatize(processed_X)
         if self.rm_stopword:
+            print("{} <Preprocess> Remove Stopword".format(datetime.now()))
             processed_X = self._rm_stop_word(processed_X)
 
         processed_Y = y.apply(lambda label: label if not label else 1)
@@ -47,26 +53,21 @@ class FAIBasePreprocessor:
         return processed_X, processed_Y
 
     def _lower(self, X:pd.Series) -> pd.Series:
-        print("{} <Preprocess> Lower (lowercase all tweets)".format(datetime.now()))
         return X.apply(lambda txt: txt.lower())
     
     def _rm_markup(self, X:pd.Series) -> pd.Series:
-        print("{} <Preprocess> Remove Markup (remove markup tags from all tweets)".format(datetime.now()))
         rgx_markup = r"<.*?>"
         return X.apply(lambda txt: sub(rgx_markup, "", txt, flags=MULTILINE))
     
     def _rm_url(self, X:pd.Series) -> pd.Series:
-        print("{} <Preprocess> Remove URL".format(datetime.now()))
         rgx_url = r"(?:https?://)?(?:www\.)?(?:[0-9][a-zA-Z0-9]+|[a-zA-Z][a-zA-Z0-9]*)(?:\.[a-zA-Z0-9]{2,}){1,}[^\s]*"
         return X.apply(lambda txt: sub(rgx_url, "", txt, flags=MULTILINE))
     
     def _rm_non_word(self, X:pd.Series) -> pd.Series:
-        print("{} <Preprocess> Remove Non-word".format(datetime.now()))
         rgx_non_word = r"[\W]"
         return X.apply(lambda txt: sub(rgx_non_word, " ", txt, flags=MULTILINE))
 
     def _lemmatize(self, X:pd.Series) -> pd.Series:
-        print("{} <Preprocess> Lemmatize (turn words into their original form)".format(datetime.now()))
         download('wordnet')
         download('punkt_tab')
         download('averaged_perceptron_tagger_eng')
@@ -86,7 +87,6 @@ class FAIBasePreprocessor:
         return X.apply(lambda txt: " ".join([stemmer.stem(lemmatizer.lemmatize(word, parse_pos(pos))) for word, pos in pos_tag(word_tokenize(txt))]))
 
     def _rm_stop_word(self, X:pd.Series) -> pd.Series:
-        print("{} <Preprocess> Remove Stopword".format(datetime.now()))
         download("stopwords")
 
         return X.apply(lambda txt: " ".join([word for word in word_tokenize(txt) if word not in stopwords.words("english")]))
